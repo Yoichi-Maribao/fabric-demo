0c9e3f407986acb1e97e2469bca0f51793512308
Yoichi-Maribao <szk.m.yo1@gmail.com>
2024-02-25 16:19:34 +0900

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,114 +1,241 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { useEffect, useRef, useState } from 'react';
+import { fabric } from 'fabric';
 
-const inter = Inter({ subsets: ["latin"] });
+type Args = {
+  file: File | null;
+};
+
+export const useGetImageUrl = ({ file }: Args) => {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+
+    let reader: FileReader | null = new FileReader();
+    reader.onloadend = () => {
+      // base64のimageUrlを生成する。
+      const base64 = reader && reader.result;
+      if (base64 && typeof base64 === 'string') {
+        setImageUrl(base64);
+      }
+    };
+    reader.readAsDataURL(file);
+
+    return () => {
+      reader = null;
+    };
+  }, [file]);
+
+  return {
+    imageUrl,
+  };
+};
 
 export default function Home() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const canvasWrapperRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
+  const [isDrawingMode, setIsDrawingMode] = useState<boolean>(false);
+  const [isTypingMode, setIsTypingMode] = useState<boolean>(false);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget?.files && e.currentTarget.files[0]) {
+      setImageFile(null);
+      const targetFile = e.currentTarget.files[0];
+      setImageFile(targetFile);
+    }
+  };
+
+  const { imageUrl } = useGetImageUrl({ file: imageFile });
+
+  useEffect(() => {
+    if (imageUrl) {
+      const _canvas = new fabric.Canvas(canvasRef.current);
+
+      _canvas.setBackgroundImage(imageUrl, (e: HTMLImageElement) => {
+        _canvas.setDimensions({width: e.width, height: e.height})
+        _canvas.renderAll()
+      })
+
+      setCanvas(_canvas);
+    }
+  }, [imageUrl])
+
+  const clickDownloadImageHandler = () => {
+    if (canvas) {
+      const dataUrl = canvas.toDataURL();
+      console.log(dataUrl)
+    }
+  }
+
+  const clearCanvas = () => {
+    if (canvas) {
+      canvas.clear();
+      console.log(imageUrl)
+      canvas.setBackgroundImage(imageUrl, (e: HTMLImageElement) => {
+        canvas.setDimensions({width: e.width, height: e.height})
+        canvas.renderAll()
+      })
+    }
+  }
+
+  const clickToggleDrawingModeHandler = () => {
+    setIsDrawingMode(!isDrawingMode);
+
+    if (canvas) {
+      canvas.isDrawingMode = !isDrawingMode;
+    }
+  }
+
+  const clickToggleTypingModeHandler = () => {
+    setIsTypingMode(!isTypingMode);
+  }
+
+  const onBlurTextareaHandler = () => {
+    if (canvas && textareaRef.current) {
+      const text = new fabric.IText(textareaRef.current.value, {
+        originX: 'center',
+        originY: 'center',
+        fill: 'red',
+      });
+      canvas.add(text);
+      text.center();
+      setIsTypingMode(false);
+    }
+  }
+
+  const clickDrawArrowHandler = () => {
+    if (canvas) {
+      const triangle = new fabric.Triangle({
+        width: 10,
+        height: 15,
+        fill: 'red',
+        left: 135,
+        top: 65,
+        angle: 90,
+      });
+
+      const line = new fabric.Line([50, 100, 100, 100], {
+        left: 75,
+        top: 69,
+        strokeWidth: 3,
+        stroke: 'red',
+      });
+
+      const arrow = new fabric.Group([line, triangle]);
+      canvas.add(arrow);
+      arrow.center();
+    }
+  }
+
+  const clickDrawCircleHandler = () => {
+    if (canvas) {
+      const circle = new fabric.Circle({
+        radius: 20,
+        stroke: 'red',
+        strokeWidth: 3,
+        fill: "transparent",
+        originX: 'center',
+        originY: 'center',
+      });
+      canvas.add(circle);
+      circle.center();
+    }
+  }
+
+  const clickDrawRectangleHandler = () => {
+    if (canvas) {
+      const rect = new fabric.Rect({
+        stroke: 'red',
+        strokeWidth: 3,
+        fill: 'transparent',
+        width: 50,
+        height: 50,
+        originX: 'center',
+        originY: 'center',
+      });
+      canvas.add(rect);
+      rect.center();
+    }
+  }
+
+  const clickDrawCrossHandler = () => {
+    if (canvas) {
+      const line1 = new fabric.Line([0, 0, 50, 50], {
+        originX: 'center',
+        originY: 'center',
+        strokeWidth: 3,
+        stroke: 'red',
+      });
+      const line2 = new fabric.Line([0, 50, 50, 0], {
+        originX: 'center',
+        originY: 'center',
+        strokeWidth: 3,
+        stroke: 'red',
+      });
+
+      const cross = new fabric.Group([line1, line2]);
+      canvas.add(cross);
+      cross.center();
+    }
+  }
+
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <div>
+        <label htmlFor="image">画像をアップロードする</label>
+        <input
+          ref={fileInputRef}
+          name="image"
+          id="image"
+          type="file"
+          accept="image/*"
+          onChange={onChangeHandler}
+        />
+      </div>
+      <p>
+        imageUrl:
+        {imageUrl && (
+          <div style={{display: "inline-block", position: "relative"}} ref={canvasWrapperRef}>
+            <canvas ref={canvasRef}/>
+            {isTypingMode && (
+              <textarea
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  transformOrigin: "center",
+                  textAlign: "center",
+                  background: "transparent",
+                  border: "none",
+                  resize: "none",
+                  outline: "none",
+                  fontSize: "40px",
+                  color: "red",
+                  fontFamily: "Times New Roman",
+                }}
+                ref={textareaRef}
+                autoFocus
+                onClick={(e) => e.stopPropagation()}
+                onBlur={() => onBlurTextareaHandler()}/>
+            )}
+            <button onClick={() => clearCanvas()} >クリア</button>
+            <button onClick={() => clickDownloadImageHandler()} >保存</button>
+            <button onClick={() => clickToggleDrawingModeHandler()} >手書きモード{isDrawingMode ? "無効" : "有効"}</button>
+            <button onClick={() => clickToggleTypingModeHandler()} >テキストモード{isTypingMode ? "無効" : "有効"}</button>
+            <button onClick={() => clickDrawArrowHandler()}>→</button>
+            <button onClick={() => clickDrawCircleHandler()}>〇</button>
+            <button onClick={() => clickDrawRectangleHandler()}>□</button>
+            <button onClick={() => clickDrawCrossHandler()}>✖</button>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        )}
+      </p>
     </>
   );
 }
